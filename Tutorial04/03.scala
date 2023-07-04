9394d59d6a3942bbfe43869b21c5a6e58613a1bb
sudo-chmod-711 <chamodhhenderson@gmail.com>
2023-07-04 05:30:14 +0530

    Update Question 03

--- Tutorial04/03.scala
+++ Tutorial04/03.scala
@@ -4,16 +4,16 @@ object Q3 {
     }
 
     def formatter(): Unit = {
-        println(formatNames("Benny")(toUpper))
-        println(formatNames("Ni")(toUpper) + "roshan")
-        println(formatNames("Saman")(toLower))
-        println("Kumar" + formatNames("a")(toUpper))
+        println(formatNames("Benny", toUpper))
+        println(formatNames("Ni", toUpper) + "roshan")
+        println(formatNames("Saman", toLower))
+        println("Kumar" + formatNames("a", toUpper))
     }
 
     def toUpper(str:String): String = str.toUpperCase()
 
     def toLower(str:String): String = str.toLowerCase()
 
-    def formatNames(name:String)(formatName: String => String): String = formatName(name)
+    def formatNames(name:String, formatName: String => String): String = formatName(name)
 
 }
